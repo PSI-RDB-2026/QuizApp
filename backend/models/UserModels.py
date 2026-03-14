729da73196d0821d32b9fa52091afefccf97585e
Jan_Reisiegel <jan.reisiegel@tul.cz>
2026-03-14 11:58:57 +0100

    UserModels

---
+++ backend/models/UserModels.py
@@ -0,0 +1,16 @@
+'''This module defines the data models for user authentication
+and token management in the QuizApp backend.
+'''
+from pydantic import BaseModel
+
+
+class LoginRequest(BaseModel):
+    '''Model for user login request. It includes the username and password'''
+    username: str
+    password: str
+
+
+class TokenResponse(BaseModel):
+    '''Model for the response returned after successful authentication.'''
+    access_token: str
+    token_type: str = "bearer"
