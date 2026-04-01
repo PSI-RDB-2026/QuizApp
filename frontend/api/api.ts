96e02b75d379b8e6011eb5d90c5c43d7c4cec4d4
Dan_Kerslager <dan.kerslager@tul.cz>
2026-04-01 13:56:04 +0200

    API routing fix

--- frontend/api/api.ts
+++ frontend/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = "http://localhost:8000";
+const apiUrl = "/api";
 
 export const postRegister = async (userParams) => {
   const { username, email, password } = userParams;
